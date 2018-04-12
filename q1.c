a6e4c530470400d5e7635f67c3c9c70d2885c636
Imazher <38215012+Imazher@users.noreply.github.com>
2018-04-12 11:56:20 +0530

    Update q1.c

--- q1.c
+++ q1.c
@@ -34,3 +34,30 @@ print_factorization (long n, factor_t * azher)
     printf (" %ld ", n);
 }
 
+
+void *
+primes_computer_runner (void *param)
+{
+  factor_t *azher;
+  long i, prime, limit;
+  thread_parameters_t *thread_parameters = (thread_parameters_t *) param;
+  pthread_mutex_lock (&thread_parameters->cond_mutex);
+  pthread_cond_wait (&thread_parameters->start_working,
+		     &thread_parameters->cond_mutex);
+
+  printf ("Thread woken up to find primes less than %ld.\n",
+	  thread_parameters->max_search);
+
+  azher =
+    (factor_t *) calloc (thread_parameters->max_search, sizeof (factor_t));
+  assert (azher != NULL);
+
+  limit = (long) sqrt (thread_parameters->max_search) + 1;
+  
+  for (prime = 2; prime <= limit; ++prime)
+    for (i = 2; i * prime < thread_parameters->max_search; ++i)
+      {
+	azher[i * prime].f[0] = i;
+	azher[i * prime].f[1] = prime;
+
+      }
